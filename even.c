a60ccae0fa6670943c1f8f7ab0aa4b6d9877332d
KOUSINISHA <35996286+KOUSINISHA@users.noreply.github.com>
2018-02-05 16:15:32 +0530

    c program
    pgrm

---
+++ even.c
@@ -0,0 +1,14 @@
+#include<stdio.h>
+void main()
+{
+int=i<1000;
+for(i=1;i<0;i++)
+printf("enter the even number");
+scanf("%d%d%\n");
+else
+printf("enter the odd number");
+scanf("%d%d%\n");
+printf(Enter the number is even or odd");
+{
+getch()
+}
